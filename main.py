3e8e54d3e25245d26aa13aa1c6f8fc4dc328a299
Sjoerd de Vries <sjoerd.de.vries@bnnvara.nl>
2023-11-05 22:37:05 +0100

    add ability to add activity

--- main.py
+++ main.py
@@ -4,56 +4,82 @@ import tkinter as tk
 from tkinter import messagebox
 import json
 
+# Define the structure for leisure activities
+activities = []
+
 
 # Initialize the main window
 def init_main_window():
     root = tk.Tk()
     root.title("Leisure Time Tracker")
 
-    # Add components to the main window here
+    # Frame for adding activities
+    add_activity_frame = tk.Frame(root)
+    add_activity_frame.pack(pady=10)
+
+    tk.Label(add_activity_frame, text="Activity Type").grid(row=0, column=0)
+    type_entry = tk.Entry(add_activity_frame)
+    type_entry.grid(row=0, column=1)
+
+    tk.Label(add_activity_frame, text="Start Time").grid(row=1, column=0)
+    start_entry = tk.Entry(add_activity_frame)
+    start_entry.grid(row=1, column=1)
+
+    tk.Label(add_activity_frame, text="End Time").grid(row=2, column=0)
+    end_entry = tk.Entry(add_activity_frame)
+    end_entry.grid(row=2, column=1)
+
+    tk.Label(add_activity_frame, text="Rating").grid(row=3, column=0)
+    rating_entry = tk.Entry(add_activity_frame)
+    rating_entry.grid(row=3, column=1)
+
+    add_button = tk.Button(
+        add_activity_frame,
+        text="Add Activity",
+        command=lambda: add_activity(type_entry, start_entry, end_entry, rating_entry),
+    )
+    add_button.grid(row=4, column=0, columnspan=2)
+
+    # Add more components to the main window here
 
     return root
 
 
 # Function to add a new leisure activity
-def add_activity():
-    # Here you would gather the data from the entry fields and process it
+def add_activity(type_entry, start_entry, end_entry, rating_entry):
     activity_type = type_entry.get()
     start_time = start_entry.get()
     end_time = end_entry.get()
     rating = rating_entry.get()
+    # Validate the inputs here
+    activities.append(
+        {"type": activity_type, "start": start_time, "end": end_time, "rating": rating}
+    )
+    save_activities()
     messagebox.showinfo(
         "Activity Added",
         f"Added {activity_type} from {start_time} to {end_time} with rating {rating}",
     )
-    # You would also call your save_activities function here
-
-
-# Function to view past leisure activities
-def view_activities():
-    # Implement the logic to view activities
-    pass
-
-
-# Function to recommend an activity based on past ratings
-def recommend_activity():
-    # Implement the logic to recommend an activity
-    pass
 
 
 # Function to save activities to a file
 def save_activities():
-    # Implement the logic to save activities
-    pass
+    with open("activities.json", "w") as f:
+        json.dump(activities, f)
 
 
 # Function to load activities from a file
 def load_activities():
-    # Implement the logic to load activities
-    pass
+    try:
+        with open("activities.json", "r") as f:
+            global activities
+            activities = json.load(f)
+    except FileNotFoundError:
+        activities = []
 
 
 # Start the GUI application
 if __name__ == "__main__":
+    load_activities()
     main_window = init_main_window()
     main_window.mainloop()
