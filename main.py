2e29ef66e81856b6b18ae9731288df687e88b7e4
Sjoerd de Vries <sjoerd.de.vries@bnnvara.nl>
2023-11-05 22:32:14 +0100

    wip adding activity

--- main.py
+++ main.py
@@ -4,42 +4,56 @@ import tkinter as tk
 from tkinter import messagebox
 import json
 
+
 # Initialize the main window
 def init_main_window():
     root = tk.Tk()
-    root.title('Leisure Time Tracker')
+    root.title("Leisure Time Tracker")
 
     # Add components to the main window here
 
     return root
 
+
 # Function to add a new leisure activity
 def add_activity():
-    # Implement the logic to add an activity
-    pass
+    # Here you would gather the data from the entry fields and process it
+    activity_type = type_entry.get()
+    start_time = start_entry.get()
+    end_time = end_entry.get()
+    rating = rating_entry.get()
+    messagebox.showinfo(
+        "Activity Added",
+        f"Added {activity_type} from {start_time} to {end_time} with rating {rating}",
+    )
+    # You would also call your save_activities function here
+
 
 # Function to view past leisure activities
 def view_activities():
     # Implement the logic to view activities
     pass
 
+
 # Function to recommend an activity based on past ratings
 def recommend_activity():
     # Implement the logic to recommend an activity
     pass
+
 
 # Function to save activities to a file
 def save_activities():
     # Implement the logic to save activities
     pass
 
+
 # Function to load activities from a file
 def load_activities():
     # Implement the logic to load activities
     pass
 
+
 # Start the GUI application
-if __name__ == '__main__':
+if __name__ == "__main__":
     main_window = init_main_window()
     main_window.mainloop()
-
