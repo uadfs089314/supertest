804609ecbd49e04de4e0ba1d261b526cbbcf6e96
Sjoerd de Vries <sjoerd.de.vries@bnnvara.nl>
2023-11-05 22:53:37 +0100

    Add readme and add a main screen

--- main.py
+++ main.py
@@ -13,53 +13,144 @@ def init_main_window():
     root = tk.Tk()
     root.title("Leisure Time Tracker")
 
-    # Frame for adding activities
-    add_activity_frame = tk.Frame(root)
-    add_activity_frame.pack(pady=10)
+    # Top section for the header
+    header_frame = tk.Frame(root)
+    header_frame.pack(pady=10)
+    tk.Label(header_frame, text="Welcome to Leisure Time Tracker").pack()
 
-    tk.Label(add_activity_frame, text="Activity Type").grid(row=0, column=0)
-    type_entry = tk.Entry(add_activity_frame)
-    type_entry.grid(row=0, column=1)
+    # Middle section for recent activities and calendar/recommendations
+    middle_frame = tk.Frame(root)
+    middle_frame.pack(pady=10)
 
-    tk.Label(add_activity_frame, text="Start Time").grid(row=1, column=0)
-    start_entry = tk.Entry(add_activity_frame)
-    start_entry.grid(row=1, column=1)
+    # Frame for recent activities list
+    recent_activities_frame = tk.Frame(middle_frame)
+    recent_activities_frame.pack(side=tk.LEFT, padx=10)
+    tk.Label(recent_activities_frame, text="Recent Activities").pack()
+    # ... (Listbox or other widgets for recent activities)
 
-    tk.Label(add_activity_frame, text="End Time").grid(row=2, column=0)
-    end_entry = tk.Entry(add_activity_frame)
-    end_entry.grid(row=2, column=1)
+    # Frame for calendar or recommendations
+    calendar_frame = tk.Frame(middle_frame)
+    calendar_frame.pack(side=tk.RIGHT, padx=10)
+    tk.Label(calendar_frame, text="Your Calendar").pack()
+    # ... (Calendar widget or recommendations list)
 
-    tk.Label(add_activity_frame, text="Rating").grid(row=3, column=0)
-    rating_entry = tk.Entry(add_activity_frame)
-    rating_entry.grid(row=3, column=1)
-
-    add_button = tk.Button(
-        add_activity_frame,
-        text="Add Activity",
-        command=lambda: add_activity(type_entry, start_entry, end_entry, rating_entry),
+    # Bottom section for action buttons
+    action_frame = tk.Frame(root)
+    action_frame.pack(pady=10)
+    tk.Button(action_frame, text="Add New Activity", command=add_activity_form).pack(
+        side=tk.LEFT
     )
-    add_button.grid(row=4, column=0, columnspan=2)
-
-    # Add more components to the main window here
+    tk.Button(action_frame, text="View Full Log", command=view_full_log).pack(
+        side=tk.LEFT
+    )
+    tk.Button(action_frame, text="Get Recommendation", command=get_recommendation).pack(
+        side=tk.LEFT
+    )
 
     return root
 
 
-# Function to add a new leisure activity
-def add_activity(type_entry, start_entry, end_entry, rating_entry):
-    activity_type = type_entry.get()
-    start_time = start_entry.get()
-    end_time = end_entry.get()
-    rating = rating_entry.get()
-    # Validate the inputs here
-    activities.append(
-        {"type": activity_type, "start": start_time, "end": end_time, "rating": rating}
-    )
-    save_activities()
+def get_recommendation():
+    if not activities:
+        messagebox.showinfo(
+            "Recommendation", "No activities found to base the recommendation on."
+        )
+        return
+
+    # Calculate the average rating for each activity type
+    ratings = {}
+    for activity in activities:
+        if activity["type"] not in ratings:
+            ratings[activity["type"]] = {"total_rating": 0, "count": 0}
+        ratings[activity["type"]]["total_rating"] += int(activity["rating"])
+        ratings[activity["type"]]["count"] += 1
+
+    # Find the activity type with the highest average rating
+    highest_avg_rating = 0
+    recommended_activity = None
+    for activity_type, rating_info in ratings.items():
+        avg_rating = rating_info["total_rating"] / rating_info["count"]
+        if avg_rating > highest_avg_rating:
+            highest_avg_rating = avg_rating
+            recommended_activity = activity_type
+
+    # Display the recommendation
     messagebox.showinfo(
-        "Activity Added",
-        f"Added {activity_type} from {start_time} to {end_time} with rating {rating}",
+        "Recommendation", f"We recommend you to enjoy some more {recommended_activity}!"
     )
+
+
+def view_full_log():
+    # Create a new top-level window
+    log_window = tk.Toplevel()
+    log_window.title("Full Activity Log")
+
+    # Create a scrollbar
+    scrollbar = tk.Scrollbar(log_window)
+    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+    # Create a Listbox and attach the scrollbar
+    log_listbox = tk.Listbox(log_window, yscrollcommand=scrollbar.set)
+    for activity in activities:
+        # Format the string as you like
+        log_listbox.insert(
+            tk.END,
+            f"{activity['type']} from {activity['start']} to {activity['end']} with rating {activity['rating']}",
+        )
+    log_listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+    # Configure the scrollbar
+    scrollbar.config(command=log_listbox.yview)
+
+
+def add_activity_form():
+    # Create a new top-level window
+    add_window = tk.Toplevel()
+    add_window.title("Add New Activity")
+
+    # Define the labels and entry widgets for the activity form
+    tk.Label(add_window, text="Activity Type").grid(row=0, column=0)
+    type_entry = tk.Entry(add_window)
+    type_entry.grid(row=0, column=1)
+
+    tk.Label(add_window, text="Start Time").grid(row=1, column=0)
+    start_entry = tk.Entry(add_window)
+    start_entry.grid(row=1, column=1)
+
+    tk.Label(add_window, text="End Time").grid(row=2, column=0)
+    end_entry = tk.Entry(add_window)
+    end_entry.grid(row=2, column=1)
+
+    tk.Label(add_window, text="Rating").grid(row=3, column=0)
+    rating_entry = tk.Entry(add_window)
+    rating_entry.grid(row=3, column=1)
+
+    # Function to handle the addition of a new activity
+    def add_activity():
+        # Here you would gather the data from the entry fields and process it
+        activity_type = type_entry.get()
+        start_time = start_entry.get()
+        end_time = end_entry.get()
+        rating = rating_entry.get()
+        # Validate the inputs here
+        activities.append(
+            {
+                "type": activity_type,
+                "start": start_time,
+                "end": end_time,
+                "rating": rating,
+            }
+        )
+        # You would also call your save_activities function here
+        messagebox.showinfo(
+            "Activity Added",
+            f"Added {activity_type} from {start_time} to {end_time} with rating {rating}",
+        )
+        add_window.destroy()  # Close the add activity window
+
+    # Button to submit the new activity
+    submit_button = tk.Button(add_window, text="Add Activity", command=add_activity)
+    submit_button.grid(row=4, column=0, columnspan=2)
 
 
 # Function to save activities to a file
---
+++ readme.md
@@ -0,0 +1,45 @@
+# Leisure Time Tracker
+
+## Overview
+
+Leisure Time Tracker is a simple GUI application built with Python's Tkinter library. It allows users to track their leisure activities, including the type of activity, start and end times, and a personal rating. The application provides a quick and easy way to log activities, view past entries, and receive recommendations based on user preferences.
+
+## Features
+
+- Add new leisure activities with details.
+- View a list of past activities.
+- Receive activity recommendations based on past ratings.
+- Data persistence through JSON file storage.
+
+## Setup
+
+To run this application, you will need Python installed on your system. If you don't have Python installed, you can download it from [python.org](https://www.python.org/downloads/).
+
+1. Clone the repository:
+   ```
+   git clone https://github.com/uadfs089314/supertest.git
+   ```
+2. Navigate to the project directory:
+   ```
+   cd supertest
+   ```
+3. Run the application:
+   ```
+   python main.py
+   ```
+
+## Usage
+
+When you launch the application, you will see the main window. From here, you can:
+
+- Click on 'Add New Activity' to open a form where you can enter the details of your leisure activity.
+- View the list of past activities in the main window.
+- Get recommendations by clicking on 'Get Recommendation' (feature to be implemented).
+
+## Contributing
+
+Contributions to the Leisure Time Tracker are welcome. Please feel free to fork the repository, make your changes, and submit a pull request.
+
+## License
+
+This project is licensed under the GNU General Public License v3.0
