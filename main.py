d9aac86b66622f8312cd69833c0ddef2b2f0f01b
Sjoerd de Vries <sjoerd.de.vries@bnnvara.nl>
2023-11-05 23:08:58 +0100

    Add date to the datamodel

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import tkinter as tk
 from tkinter import messagebox
+from tkcalendar import Calendar
 import json
 
 # Define the structure for leisure activities
@@ -28,11 +29,52 @@ def init_main_window():
     tk.Label(recent_activities_frame, text="Recent Activities").pack()
     # ... (Listbox or other widgets for recent activities)
 
+    # Create a scrollbar
+    scrollbar = tk.Scrollbar(recent_activities_frame)
+    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+    # Create a Listbox and attach the scrollbar
+    listbox = tk.Listbox(recent_activities_frame, yscrollcommand=scrollbar.set)
+    listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+    # Configure the scrollbar
+    scrollbar.config(command=listbox.yview)
+
+    # Function to populate the Listbox with recent activities
+    def populate_listbox():
+        # Clear the Listbox
+        listbox.delete(0, tk.END)
+
+        # Sort activities by date, assuming 'date' is in 'YYYY-MM-DD' format
+        sorted_activities = sorted(activities, key=lambda x: x["date"], reverse=True)
+
+        # Insert the last 10 activities or all if less than 10
+        for activity in sorted_activities[:10]:
+            listbox.insert(
+                tk.END,
+                f"{activity['date']}: {activity['type']} from {activity['start']} to {activity['end']} (Rating: {activity['rating']})",
+            )
+
+    # Call the function to populate the Listbox
+    populate_listbox()
+
     # Frame for calendar or recommendations
     calendar_frame = tk.Frame(middle_frame)
     calendar_frame.pack(side=tk.RIGHT, padx=10)
     tk.Label(calendar_frame, text="Your Calendar").pack()
     # ... (Calendar widget or recommendations list)
+
+    cal = Calendar(calendar_frame, selectmode="day", year=2023, month=11, day=5)
+    cal.pack(pady=20, padx=20, fill="both", expand=True)
+
+    # Function to display selected date or activities
+    def show_date(event):
+        selected_date = cal.get_date()
+        # You can add functionality to show activities for the selected date
+        print(f"Selected Date is: {selected_date}")
+
+    # Bind the calendar event to show_date function
+    cal.bind("<<CalendarSelected>>", show_date)
 
     # Bottom section for action buttons
     action_frame = tk.Frame(root)
@@ -95,7 +137,7 @@ def view_full_log():
         # Format the string as you like
         log_listbox.insert(
             tk.END,
-            f"{activity['type']} from {activity['start']} to {activity['end']} with rating {activity['rating']}",
+            f"{activity['date']}: {activity['type']} from {activity['start']} to {activity['end']} with rating {activity['rating']}",
         )
     log_listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
@@ -121,6 +163,10 @@ def add_activity_form():
     end_entry = tk.Entry(add_window)
     end_entry.grid(row=2, column=1)
 
+    tk.Label(add_window, text="Date").grid(row=4, column=0)
+    date_entry = tk.Entry(add_window)
+    date_entry.grid(row=4, column=1)
+
     tk.Label(add_window, text="Rating").grid(row=3, column=0)
     rating_entry = tk.Entry(add_window)
     rating_entry.grid(row=3, column=1)
@@ -131,6 +177,7 @@ def add_activity_form():
         activity_type = type_entry.get()
         start_time = start_entry.get()
         end_time = end_entry.get()
+        date = date_entry.get()
         rating = rating_entry.get()
         # Validate the inputs here
         activities.append(
@@ -138,6 +185,7 @@ def add_activity_form():
                 "type": activity_type,
                 "start": start_time,
                 "end": end_time,
+                "date": date,
                 "rating": rating,
             }
         )
